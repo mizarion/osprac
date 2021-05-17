e02b0bfe5a59bd4939fa243d57dfe1fd26e7e3f4
mizarion <zsgluschenko@edu.hse.ru>
2021-05-17 17:21:00 +0300

    fix

--- Lab12/Client.c
+++ Lab12/Client.c
@@ -6,76 +6,63 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define LAST_MESSAGE 255
+struct mymsgbuf
+{
+	long mtype;
+	float num;
+} mybuf;
 
 int main(void)
 {
-    int msqid;
-    char pathname[]="Client.c";
-    key_t  key;
-    int len, maxlen;
+	char pathname[] = "Client.c";
+	key_t  key;
+	if ((key = ftok(pathname, 0)) < 0)
+	{
+		printf("Can\'t generate key\n");
+		exit(-1);
+	}
 
-    struct mymsgbuf
-    {
-        long mtype;
-        float mnumber;
-    } mybuf;
+	int msqid;
+	if ((msqid = msgget(key, 0666 | IPC_CREAT)) < 0)
+	{
+		printf("Can\'t get msqid\n");
+		exit(-1);
+	}
 
-    if ((key = ftok(pathname,0)) < 0) {
-        printf("Can\'t generate key\n");
-        exit(-1);
-    }
+	float n;
+	printf("Input: ");
+	scanf("%f", &n);
 
-    if ((msqid = msgget(key, 0666 | IPC_CREAT)) < 0) {
-        printf("Can\'t get msqid\n");
-        exit(-1);
-    }
+	mybuf.mtype = 1;
+	mybuf.num = n;
+	int len = sizeof(mybuf.num);
 
-	/* Read message. */
-	float message;
-	printf("Input >> ");
-	scanf("%f", &message);
-
-	/* Send to server. */
-	
-	//
-    // Fill in the structure for the message and
-    // determine the length of the informative part.
-    //
-    mybuf.mtype = 1;
-    mybuf.mnumber = message;
-    len = sizeof(mybuf.mnumber);
-      
-    //
-    // Send the message. If there is an error,
-    // report it and delete the message queue from the system.
-    //
-    if (msgsnd(msqid, (struct msgbuf *) &mybuf, len, 0) < 0) {
-      printf("Can\'t send mess to server\n");
-      msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
-      exit(-1);
-    }
-
-	/* Receive */
-	maxlen = sizeof(float);
-
-    if (( len = msgrcv(msqid, (struct msgbuf *) &mybuf, maxlen, 1, 0)) < 0) {
-      printf("Can\'t receive message from server\n");
-      exit(-1);
-    }
-    printf("type = %ld, info = %f\n", mybuf.mtype, mybuf.mnumber);
-
-	/* LAST MESSAGE */
-	mybuf.mtype = LAST_MESSAGE;
-	len = 0;
-
-	if (msgsnd(msqid, (struct msgbuf*)&mybuf, len, 0) < 0) {
-		printf("Can\'t send mess to server\n");
+	if (msgsnd(msqid, (struct msgbuf*)&mybuf, len, 0) < 0)
+	{
+		printf("Can\'t send message to server\n");
 		msgctl(msqid, IPC_RMID, (struct msqid_ds*)NULL);
 		exit(-1);
 	}
 
-    msgctl(msqid, IPC_RMID, (struct msqid_ds *)NULL);
+	int maxlen = sizeof(float);
+	if ((len = msgrcv(msqid, (struct msgbuf*)&mybuf, maxlen, 1, 0)) < 0)
+	{
+		printf("Can\'t receive message from server\n");
+		exit(-1);
+	}
+	printf("type = %ld, info = %f\n", mybuf.mtype, mybuf.num);
 
-    return 0;
+	mybuf.mtype = 255;
+	len = 0;
+
+	if (msgsnd(msqid, (struct msgbuf*)&mybuf, len, 0) < 0)
+	{
+		printf("Can\'t send message to server\n");
+		msgctl(msqid, IPC_RMID, (struct msqid_ds*)NULL);
+		exit(-1);
+	}
+
+	msgctl(msqid, IPC_RMID, (struct msqid_ds*)NULL);
+
+	return 0;
 }
